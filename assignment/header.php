6751139800b5ee09aec7e93a12854ad9fdab4620
YasiruDB <yasirudewminabandara@gmail.com>
2025-06-06 16:17:47 +0530

    Add files via upload

---
+++ assignment/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>YD Article</title>
+  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+<link rel="stylesheet" href="style.css">
+
+</head>
+
+  <!-- Header Section -->
+  <header class="bg-gray-800 text-white py-4">
+    <div class="flex justify-center">
+      <img src="images/articale.jpg" alt="articale.jpg" class="h-24 rounded">
+    </div>
+  </header>
+
+  <body>
+
+  <div class="container header bg-[#d62939] px-[20px] py-[12px] font-serif flex justify-between">
+        <a href="addarticle.php" class="text-center text-2xl text-decoration-none  text-white" > YD Article </a>
+        <a href="viewarticle.php" class="bg-black hover:bg-white hover:text-black py-1 px-3 text-decoration-none  text-white">Add Article Details</a>
+    </div> 
+
+</body>
+</html>
+
+
